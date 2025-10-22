6af0f53c6c30a0124af09f93dafeea74316bcc13
Something Missing <212981488+SomegitMissing@users.noreply.github.com>
2025-10-22 16:30:16 -0400

    🔥Cleaning

--- canvas.gd
+++ canvas.gd
@@ -1,25 +1,8 @@
 class_name Canvas
 extends Camera2D
 
-@export var particles: int;
-@export var radius: float;
-
-var p_arr: Array[Trajectory] = [];
-
 func _ready() -> void:
-	RenderingServer.viewport_set_clear_mode(
-		get_viewport().get_viewport_rid(),
-		RenderingServer.VIEWPORT_CLEAR_NEVER
-	);
-	var pi_div_p := TAU / particles;
-
-	for i in particles:
-		var p := Trajectory.new();
-		p.position.x = cos(i * pi_div_p)*radius;
-		p.position.y = sin(i * pi_div_p)*radius;
-		p_arr.append(p);
-		add_child(p);
-
+	pass
 
 func _process(_delta: float) -> void:
 	queue_redraw()
@@ -36,39 +19,4 @@ func fill(color: Color):
 
 
 func _draw() -> void:
-	fill(Color(0, 0, 0, 5.0/255));
-
-	var frame_count := Engine.get_process_frames();
-	for i in particles:
-		var i_pi_div := float(i) / particles * PI;
-		var curvature := 0.7 + cos(i_pi_div + frame_count * 0.1)/5;
-		var osc := cos(i_pi_div + frame_count * 0.01) + 0.2;
-
-		var p := p_arr[i];
-		if i > 0:
-			var prev_p := p_arr[i-1];
-			p.foward();
-			p.steer_towards(prev_p.position, curvature);
-			p.average_color(prev_p, 0.95);
-		else:
-			var prev_p := p_arr[particles - 1];
-			p.foward()
-			p.steer_towards(prev_p.position, curvature);
-
-
-		if p.mutant:
-			p.step_magnitude = osc;
-
-	if randi_range(0, 50) != 1:
-		return;
-
-	var r_i := randi_range(0, particles-1);
-
-	var swap = p_arr[0];
-	p_arr[0] = p_arr[r_i];
-	p_arr[r_i] = swap;
-
-	var p0 := p_arr[0];
-
-	p0.line_color = Color(randf(), randf(), randf());
-	p0.mutant = true;
+	pass
