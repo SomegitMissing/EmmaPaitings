c8d5da20d847dca8c4db26b44f880cf27ddfad4e
Something Missing <212981488+SomegitMissing@users.noreply.github.com>
2025-10-22 20:40:34 -0400

    ✨Making the main branch clean

--- canvas.gd
+++ canvas.gd
@@ -2,21 +2,26 @@ class_name Canvas
 extends Camera2D
 
 func _ready() -> void:
-	pass
+	RenderingServer.viewport_set_clear_mode(
+		get_viewport().get_viewport_rid(),
+		RenderingServer.VIEWPORT_CLEAR_NEVER,
+	);
+
+func camera_size() -> Vector2:
+	return get_viewport_rect().size / zoom;
 
 func _process(_delta: float) -> void:
 	queue_redraw()
 
 func fill(color: Color):
-	var camera_size := get_viewport_rect().size / zoom;
+	var cam_size := camera_size();
 
 	draw_rect(Rect2(
-		position.x - camera_size.x/2,
-		position.y - camera_size.y/2,
-		camera_size.x,
-		camera_size.y,
+		position.x - cam_size.x/2,
+		position.y - cam_size.y/2,
+		cam_size.x,
+		cam_size.y,
 	), color);
-
 
 func _draw() -> void:
 	pass
