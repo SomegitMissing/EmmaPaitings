13e2a2e8a50c71d72f4e8687e4b7c0378a30a433
Something Missing <212981488+SomegitMissing@users.noreply.github.com>
2025-10-30 12:16:40 -0400

    ✨Adding frame count

--- canvas.gd
+++ canvas.gd
@@ -1,6 +1,8 @@
 class_name Canvas
 extends Camera2D
 
+static var frame_count: int;
+
 func _ready() -> void:
 	RenderingServer.viewport_set_clear_mode(
 		get_viewport().get_viewport_rid(),
@@ -8,6 +10,7 @@ func _ready() -> void:
 	);
 
 func _process(_delta: float) -> void:
+	frame_count = Engine.get_process_frames();
 	queue_redraw()
 
 func fill(color: Color):
