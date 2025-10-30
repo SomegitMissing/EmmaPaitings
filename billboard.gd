0fc7c6bed4af752d3434c19478f7219154602b17
Something Missing <212981488+SomegitMissing@users.noreply.github.com>
2025-10-30 13:48:51 -0400

    ✨Creating fireworks

---
+++ billboard.gd
@@ -0,0 +1,38 @@
+class_name Billboard
+extends Node2D
+
+var f: float = 1;
+var stroke_color: Color = Color.from_rgba8(255, 150, 0);
+
+func _process(_delta: float) -> void:
+	queue_redraw()
+
+func _draw() -> void:
+	for i in 300:
+		var circle_pos := Vector2(
+			0.2 * cos(f * 0.999 * i + Canvas.frame_count * 0.01) * i,
+			0.2 * sin(f * i + Canvas.frame_count * 0.01) * i
+		);
+
+		var radius := cos(Canvas.frame_count * 0.001 * i) + 5;
+
+		draw_circle(
+			circle_pos,
+			radius + 1,
+			stroke_color
+		);
+		draw_circle(
+			circle_pos,
+			radius,
+			Color.WHITE
+		);
+		draw_rect(
+			Rect2(
+				Vector2(
+					0.5 * sin(f * 0.999 * i + Canvas.frame_count * 0.01) * i,
+					0.5 * cos(f * i + Canvas.frame_count * 0.01) * i
+				),
+				Vector2.ONE
+			),
+			stroke_color,
+		);
--- canvas.gd
+++ canvas.gd
@@ -1,13 +1,18 @@
 class_name Canvas
 extends Camera2D
 
+@export var billboard: Billboard;
+
 static var frame_count: int;
+
+var amplitude: float = 1;
 
 func _ready() -> void:
 	RenderingServer.viewport_set_clear_mode(
 		get_viewport().get_viewport_rid(),
 		RenderingServer.VIEWPORT_CLEAR_NEVER,
 	);
+	Engine.max_fps = 60;
 
 func _process(_delta: float) -> void:
 	frame_count = Engine.get_process_frames();
@@ -24,4 +29,18 @@ func fill(color: Color):
 	), color);
 
 func _draw() -> void:
-	pass
+	if cos(frame_count*0.1) < -0.999:
+		billboard.f = randf() * 5;
+		var viewport_size := (get_viewport_rect().size / 2) - Vector2.ONE * 100;
+
+		billboard.position = Vector2(
+			randf_range(-viewport_size.x, viewport_size.x),
+			randf_range(-viewport_size.y, viewport_size.y),
+		);
+
+	fill(Color.from_rgba8(0, 0, 0, 10));
+
+	billboard.rotation = frame_count * 0.001 * cos(frame_count*0.01);
+	billboard.scale = amplitude * Vector2.ONE;
+
+	amplitude = 0.5 * cos(frame_count * 0.1) + 1;
