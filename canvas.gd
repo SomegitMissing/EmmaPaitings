7aff468d537a3b4e261d54ab2b989325581b3ce0
Something Missing <212981488+SomegitMissing@users.noreply.github.com>
2025-11-01 12:14:22 -0400

    ✨Iteration one

--- canvas.gd
+++ canvas.gd
@@ -3,25 +3,13 @@ extends Camera2D
 
 static var frame_count: int;
 
-@export var particles: int;
-@export var radius: float;
-
-var p_arr: Array[Trajectory] = [];
+@export var margin: float = 100;
 
 func _ready() -> void:
 	RenderingServer.viewport_set_clear_mode(
 		get_viewport().get_viewport_rid(),
 		RenderingServer.VIEWPORT_CLEAR_NEVER
 	);
-	var pi_div_p := TAU / particles;
-
-	for i in particles:
-		var p := Trajectory.new();
-		p.position.x = cos(i * pi_div_p)*radius;
-		p.position.y = sin(i * pi_div_p)*radius;
-		p_arr.append(p);
-		add_child(p);
-
 
 func _process(_delta: float) -> void:
 	frame_count = Engine.get_process_frames();
@@ -36,38 +24,67 @@ func fill(color: Color):
 	), color);
 
 func _draw() -> void:
-	fill(Color.from_rgba8(0, 0, 0, 5));
+	fill(Color.from_rgba8(0, 0, 0, 10));
+	var viewport_size := get_viewport_rect().size / 2;
+	var rand_pos := Vector2(
+		randf_range(-viewport_size.x + margin, viewport_size.x - margin),
+		viewport_size.y,
+	);
 
-	for i in particles:
-		var i_pi_div := float(i) / particles * PI;
-		var curvature := 0.7 + cos(i_pi_div + frame_count * 0.1)/5;
-		var osc := cos(i_pi_div + frame_count * 0.01) + 0.2;
+	if randi_range(0, 70) == 0:
+		var traject := Trajectory.new();
+		traject.position = rand_pos;
+		traject.direction = deg_to_rad(270);
+		traject.ttl = randf_range(0.5, 1);
 
-		var p := p_arr[i];
-		if i > 0:
-			var prev_p := p_arr[i-1];
-			p.foward();
-			p.steer_towards(prev_p.position, curvature);
-			p.average_color(prev_p, 0.95);
-		else:
-			var prev_p := p_arr[particles - 1];
-			p.foward()
-			p.steer_towards(prev_p.position, curvature);
+		add_child(traject);
+
+	var child_count := get_child_count();
+	for child_i in child_count:
+		var particle: Trajectory = get_child(child_i);
+
+		particle.foward();
+		match particle.generation:
+			0:
+				particle.direction = deg_to_rad(270 + cos(Canvas.frame_count * 0.1) * 10);
+			1:
+				particle.steer(deg_to_rad(90), 0.01);
+			2:
+				particle.steer(deg_to_rad(90), 0.03);
 
 
-		if p.mutant:
-			p.step_magnitude = osc;
+	# fill(Color.from_rgba8(0, 0, 0, 5));
 
-	if randi_range(0, 50) != 1:
-		return;
+	# for i in particles:
+	# 	var i_pi_div := float(i) / particles * PI;
+	# 	var curvature := 0.7 + cos(i_pi_div + frame_count * 0.1)/5;
+	# 	var osc := cos(i_pi_div + frame_count * 0.01) + 0.2;
 
-	var r_i := randi_range(0, particles-1);
+	# 	var p := particle_arr[i];
+	# 	if i > 0:
+	# 		var prev_p := particle_arr[i-1];
+	# 		p.foward();
+	# 		p.steer_towards(prev_p.position, curvature);
+	# 		p.average_color(prev_p, 0.95);
+	# 	else:
+	# 		var prev_p := particle_arr[particles - 1];
+	# 		p.foward()
+	# 		p.steer_towards(prev_p.position, curvature);
 
-	var swap = p_arr[0];
-	p_arr[0] = p_arr[r_i];
-	p_arr[r_i] = swap;
 
-	var p0 := p_arr[0];
+	# 	if p.mutant:
+	# 		p.step_magnitude = osc;
 
-	p0.line_color = Color(randf(), randf(), randf());
-	p0.mutant = true;
+	# if randi_range(0, 50) != 1:
+	# 	return;
+
+	# var r_i := randi_range(0, particles-1);
+
+	# var swap = particle_arr[0];
+	# particle_arr[0] = particle_arr[r_i];
+	# particle_arr[r_i] = swap;
+
+	# var p0 := particle_arr[0];
+
+	# p0.line_color = Color(randf(), randf(), randf());
+	# p0.mutant = true;
--- trajectory.gd
+++ trajectory.gd
@@ -4,17 +4,30 @@ extends Node2D
 var delta_pos: Vector2;
 var direction: float = 0;
 var step_magnitude: float = 3;
-var mutant: bool = false;
 var line_color: Color = Color.WHITE;
-var drw_line: Callable;
-var drw_circle: Callable;
+var generation: int = 0;
+
+var parent: Node2D;
+
+var ttl: float;
+var timer: Timer;
+
+const particle_amount := [25, 10];
 
 func _ready() -> void:
+	parent = get_parent();
 	delta_pos = position;
 
-	var parent = get_parent();
-	drw_line = parent.draw_line;
-	drw_circle = parent.draw_circle;
+	timer = Timer.new();
+	timer.autostart = false;
+	timer.wait_time = ttl;
+
+	timer.one_shot = true;
+
+	add_child(timer);
+
+	timer.start(ttl);
+
 
 func average_color(target: Trajectory, percentage: float) -> void:
 	line_color = line_color.lerp(target.line_color, percentage);
@@ -23,7 +36,7 @@ func foward():
 	delta_pos.x += cos(direction) * step_magnitude;
 	delta_pos.y += sin(direction) * step_magnitude;
 
-	drw_line.call(
+	parent.draw_line(
 		position,
 		delta_pos,
 		line_color,
@@ -31,9 +44,30 @@ func foward():
 		true
 	);
 
-	if mutant:
-		drw_circle.call(position, 3, line_color)
+	if timer.time_left <= 0:
+		parent.draw_circle(position, 15.0 / (generation + 1), Color.WHITE);
+		queue_free();
 
+		if generation > 1:
+			return;
+
+		for _i in particle_amount[generation]:
+			var traject := Trajectory.new();
+			traject.generation = generation + 1;
+
+			if generation == 0:
+				traject.ttl = randf_range(0.2, 0.4);
+				traject.line_color = Color.from_hsv(randf()*0.2, 1, 1);
+			elif generation == 1:
+				traject.ttl = randf_range(0.05, 0.1);
+				traject.line_color = Color.from_hsv(randf()*0.2 + 0.5, 1, 1);
+
+			traject.direction = deg_to_rad(randf_range(0, 360));
+			traject.position = position;
+
+			get_parent().add_child(traject);
+
+		return;
 
 	position = delta_pos;
 
@@ -46,4 +80,10 @@ func steer_towards(target: Vector2, steering_magnitude: float):
 	direction = atan2(
 		lerp(sin(direction), sin(target_dir), steering_magnitude),
 		lerp(cos(direction), cos(target_dir), steering_magnitude),
+	);
+
+func steer(angle: float, steering_magnitude: float):
+	direction = atan2(
+		lerp(sin(direction), sin(angle),steering_magnitude),
+		lerp(cos(direction), cos(angle),steering_magnitude)
 	);
